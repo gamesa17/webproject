1bc99eff506c5a141f81ac70ffdbb04ca176df8e
po1yana <104688678+po1yana@users.noreply.github.com>
2022-05-13 14:12:49 +0300

    styles

--- web-01/global/style-inlet.js
+++ web-01/global/style-inlet.js
@@ -31,30 +31,16 @@ function ThemeSwitch() {
 document.querySelectorAll('.scaleable__image').forEach(imageDiv => {
     imageDiv.addEventListener('click', (event) => {
 
-      if(event.target.classList.contains("scaleable__image-selected")){
-        document.querySelectorAll('.scaleable__image').forEach(imageDiv => imageDiv.classList.remove("scaleable__image-selected"))
-            
-        }else{
+        if(event.target.classList.contains("scaleable__image-selected")){
+        document.querySelectorAll('.scaleable__image').forEach(imageDiv => imageDiv.classList.remove("scaleable__image-selected"))   
+        }
+        else{
             document.querySelectorAll('.scaleable__image').forEach(imageDiv => imageDiv.classList.remove("scaleable__image-selected"))
             event.target.classList.add("scaleable__image-selected")
         }
-      
+        
     })
 })
-
- function imageScaleOnClick(image){
-    if(image.style.alignSelf == 'center'){
-        image.style.alignSelf = 'baseline';
-        image.style.justifySelf = 'baseline';
-        image.style.position ='static';
-
-    }
-    else {
-        image.style.alignSelf = 'center';
-        image.style.justifySelf = 'center';
-        image.style.position ='absolute';
-    }
- }
 
 
 ThemeSwitch();
