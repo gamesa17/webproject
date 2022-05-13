a700d04eacb67a245a967b6c3d5a3317718c0d71
po1yana <104688678+po1yana@users.noreply.github.com>
2022-05-14 01:14:36 +0300

    big update profile

--- web-01/profile/profile.js
+++ web-01/profile/profile.js
@@ -3,6 +3,7 @@ var editname_input = document.getElementById('editname-input')
 var editname_btn = document.getElementById('editname-btn')
 var settings = document.getElementById('settings')
 var settings_body = document.getElementById('settings-body')
+var name_edit_container = document.getElementById('name-edit-container')
 
 var IsNameEdit = true;
 var IsSettingsOpen = true
@@ -10,18 +11,28 @@ var IsSettingsOpen = true
 function EditName(){
     if(IsNameEdit){
         editname_input.style.visibility = 'hidden';
+
+        name_edit_container.style.visibility = 'hidden';
+        name_edit_container.style.width = '0';
+
         profile_name.style.visibility = 'visible';
         profile_name.style.width = 'initial';
         editname_btn.getElementsByClassName('btn-img').item(0).style.visibility = 'visible';
+        editname_btn.getElementsByClassName('btn-img').item(0).style.height= '30px';
         editname_btn.getElementsByClassName('btn-img').item(1).style.visibility = 'hidden';
         IsNameEdit = false;
     }
     else{
         editname_input.style.visibility = 'visible';
+
+        name_edit_container.style.visibility = 'visible';
+        name_edit_container.style.width = 'initial';
+
         profile_name.style.visibility = 'hidden';
         profile_name.style.width = 0;
         editname_btn.getElementsByClassName('btn-img').item(1).style.visibility = 'visible';
         editname_btn.getElementsByClassName('btn-img').item(0).style.visibility = 'hidden';
+        editname_btn.getElementsByClassName('btn-img').item(0).style.height= '0';
         IsNameEdit = true;
     }
     console.log('IN')
@@ -34,8 +45,8 @@ function closeUpSettings(btn){
         IsSettingsOpen = false;
     }
     else {
-        settings_body.style.height = '90vh';
-        settings.style.height = '140vh';
+        settings_body.style.height = 'var(--settings-body-height)';
+        settings.style.height = 'var(--settings-height)';
         IsSettingsOpen = true;
     }
 }
